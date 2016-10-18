f54ca34317122c3a214a572bf57bdffb85b638b3
ruihui0821 <ruihui0821@users.noreply.github.com>
2016-10-18 11:55:09 -0700

    Update us_policy_capita.sql

--- us_policy_capita.sql
+++ us_policy_capita.sql
@@ -35,7 +35,9 @@ create table us.policy_monthly_2015_llj_pop10
 as select
 llj_id,
 year,
+year + 1 as endyear,
 month,
+month - 1 as endmonth,
 ljp.pop10 as population,
 count/ljp.pop10 as count_capita,
 t_premium/ljp.pop10 as t_premium_capita,
@@ -69,21 +71,20 @@ join fima.llj_population ljp using (llj_id);
 -- mencoder US_Accumulative_Monthly_#claims_capita.avi -o US_Accumulative_Monthly_#claims_capita.wmv -of lavf -oac lavc -ovc lavc \ -lavcopts vcodec=wmv1:vbitrate=1500:acodec=wmav1 (not work)
 -- mencoder "mf://*.png" -mf fps=5 -o US_Accumulative_Monthly_#claims_capita.wmv -ovc lavc -lavcopts vcodec=wmv2
 
-drop table us.claims_accum_monthly_2015_llj_pop10;
-create table us.claims_accum_monthly_2015_llj_pop10
+drop table us.policy_effmonthly_2015_llj_pop10;
+create table us.policy_effmonthly_2015_llj_pop10
 as select
 llj_id,
 year,
-year + 1 as endyear,
+endyear,
 month,
-month - 1 as endmonth,
+endmonth,
 sum(count_capita) OVER(PARTITION BY llj_id ORDER BY endyear, endmonth) - sum(count_capita) OVER(PARTITION BY llj_id ORDER BY year, month) AS accu_count_capita,
-sum(t_premium) OVER(PARTITION BY llj_id ORDER BY year, month) AS accu_t_premium_capita,
+sum(t_premium_capita) OVER(PARTITION BY llj_id ORDER BY year, month) AS accu_t_premium_capita,
 sum(t_cov_bldg_capita) OVER(PARTITION BY llj_id ORDER BY year, month) AS accu_t_cov_bldg_capita,
 sum(t_cov_cont_capita) OVER(PARTITION BY llj_id ORDER BY year, month) AS accu_t_cov_cont_capita,
 sum(t_cov_capita) OVER(PARTITION BY llj_id ORDER BY year, month) AS accu_t_cov_capita,
 epoch_start,
 epoch_end,
-accu_epoch_end,
 boundary
-from us.claims_monthly_2015_llj_pop10;
+from us.policy_monthly_2015_llj_pop10;
