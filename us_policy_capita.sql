bf45f8e5be5d1669027f0e7a96753dffa5a48de7
ruihui0821 <ruihui0821@users.noreply.github.com>
2016-10-19 12:27:31 -0700

    Update us_policy_capita.sql

--- us_policy_capita.sql
+++ us_policy_capita.sql
@@ -20,23 +20,23 @@ join fima.jurisdictions j using (jurisdiction_id)
 join fima.lljpolicy_population ljp using (llj_id)
 where year>=1994 and year<=2014;
 
--- cd Downloads/Time_Manager/US_yearly_#claims_capita
+-- cd Downloads/Time_Manager/US_Annual_#policy_capita
 
--- mencoder mf://*.png -mf w=480:h=640:fps=1:type=png -ovc copy -oac copy -o US_Annual_claims_number_capita.mp4
+-- mencoder mf://*.png -mf w=480:h=640:fps=1:type=png -ovc copy -oac copy -o US_Annual_#policy_capita.mp4
 
--- cd Downloads/Time_Manager/US_yearly_payclaims_capita
+-- cd Downloads/Time_Manager/US_Annual_premiumpolicy_capita
 
--- mencoder mf://*.png -mf w=640:h=480:fps=1:type=png -ovc copy -oac copy -o US_Annual_claims_payments_capita.mp4
+-- mencoder mf://*.png -mf w=640:h=480:fps=1:type=png -ovc copy -oac copy -o US_Annual_premiumpolicy_capita.mp4
 
 
 
-drop table us.policy_monthly_2015_llj_pop10;
-create table us.policy_monthly_2015_llj_pop10
+drop table us.policy_monthlynew_2015_llj_pop10;
+create table us.policy_monthlynew_2015_llj_pop10
 as select
 llj_id,
 year,
-year - 1 as syear,
 month,
+year - 1 as syear,
 ljp.pop10 as population,
 count/ljp.pop10 as count_capita,
 t_premium/ljp.pop10 as t_premium_capita,
@@ -49,29 +49,19 @@ lj.boundary
 from summary.policy_monthly_summary_2015_llj s
 join fima.lljpolicy lj using (llj_id)
 join fima.jurisdictions j using (jurisdiction_id)
-join fima.lljpolicy_population ljp using (llj_id);
+join fima.lljpolicy_population ljp using (llj_id) where llj_id <= 1000;
 
--- cd Downloads/Time_Manager/US_monthly_#claims_kcapita
+-- cd Downloads/Time_Manager/US_monthlynew_#policy_capita
 
--- mencoder mf://*.png -mf w=480:h=640:fps=1:type=png -ovc copy -oac copy -o US_Monthly_claims_number_kcapita.mp4
+-- mencoder mf://*.png -mf w=480:h=640:fps=1:type=png -ovc copy -oac copy -o US_Monthly_new_#policy_capita.mp4
 
--- cd Downloads/Time_Manager/US_monthly_payclaims_kcapita
+-- cd Downloads/Time_Manager/US_monthlynew_premiumpolicy_capita
 
--- mencoder mf://*.png -mf w=640:h=480:fps=1:type=png -ovc copy -oac copy -o US_Monthly_claims_payments_kcapita.mp4
+-- mencoder mf://*.png -mf w=640:h=480:fps=1:type=png -ovc copy -oac copy -o US_Monthly_new_premiumpolicy_capita.mp4
 
--- cd Downloads/Time_Manager/US_accumulative_monthly_#claims_capita
 
--- Creating a Motion PNG (MPNG) file from all the PNG files in the current directory:
--- mencoder mf://*.png -mf w=640:h=480:fps=5:type=png -ovc copy -oac copy -o US_Accumulative_Monthly_#claims_capita.mp4
-
--- Creating an uncompressed file from all the PNG files in the current directory:
--- mencoder mf://*.png -mf w=640:h=480:fps=5:type=png -ovc raw -oac copy -o US_Accumulative_Monthly_#claims_capita.mp4
--- convert to high quality but large size
--- mencoder US_Accumulative_Monthly_#claims_capita.avi -o US_Accumulative_Monthly_#claims_capita.wmv -of lavf -oac lavc -ovc lavc \ -lavcopts vcodec=wmv1:vbitrate=1500:acodec=wmav1 (not work)
--- mencoder "mf://*.png" -mf fps=5 -o US_Accumulative_Monthly_#claims_capita.wmv -ovc lavc -lavcopts vcodec=wmv2
-
-drop table us.policy_effmonthly_2015_llj_pop10;
-create table us.policy_effmonthly_2015_llj_pop10
+drop table us.policy_monthlyeff_2015_llj_pop10;
+create table us.policy_monthlyeff_2015_llj_pop10
 as select
 llj_id,
 year,
@@ -85,4 +75,65 @@ sum(t_cov_capita) OVER(PARTITION BY llj_id ORDER BY year, month) - sum(t_cov_cap
 epoch_start,
 epoch_end,
 boundary
-from us.policy_monthly_2015_llj_pop10;
+from us.policy_monthlynew_2015_llj_pop10
+where year>=1994 and year<=2014;
+
+-- cd Downloads/Time_Manager/US_monthlyeff_#policy_capita
+
+-- mencoder mf://*.png -mf w=480:h=640:fps=1:type=png -ovc copy -oac copy -o US_Monthly_effective_#policy_capita.mp4
+
+-- cd Downloads/Time_Manager/US_monthlyeff_premiumpolicy_capita
+
+-- mencoder mf://*.png -mf w=640:h=480:fps=1:type=png -ovc copy -oac copy -o US_Monthly_effective_premiumpolicy_capita.mp4
+
+with t as (
+  select
+  year,
+  month
+  from us.policy_monthlynew_2015_llj_pop10
+  group by 1,2
+  order by 1,2),
+s as (
+  select
+  llj_id,
+  year as year,
+  month,
+  sum(count_capita) OVER(PARTITION BY llj_id ORDER BY year, month) AS scount
+  from us.policy_monthlynew_2015_llj_pop10),
+e as (select
+  llj_id,
+  year + 1 as year,
+  month,
+  sum(count_capita) OVER(PARTITION BY llj_id ORDER BY year, month) AS ecount
+  from us.policy_monthlynew_2015_llj_pop10)
+select 
+j.llj_id,
+t.year AS year,
+t.month AS month,
+s.scount,
+e.ecount,
+s.scount - e.ecount as count_eff
+from us.policy_monthlynew_2015_llj_pop10 j 
+full join t using(year, month)
+full join s using(llj_id, year, month)
+full join e using(llj_id, year, month)
+where llj_id = 1 order by year, month;
+      
+     
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
