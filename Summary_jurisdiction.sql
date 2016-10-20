6d7f3ff648667f0facd53f899c13d29f88583abf
ruihui0821 <ruihui0821@users.noreply.github.com>
2016-10-19 21:43:21 -0700

    Update Summary_jurisdiction.sql

--- Summary_jurisdiction.sql
+++ Summary_jurisdiction.sql
@@ -1,58 +1,59 @@
 set search_path=summary,fima,public,us,ca;
--- ratios of policy premium to claims payment by state
--- according to the re_state column
-drop table us.premium_pay_state;
-create table us.premium_pay_state as
+-- ratios of policy premium to claims payment by jurisdiction
+-- according to the re_state and jurisdiction_id column
+drop table us.premium_pay_jurisdiction;
+create table us.premium_pay_jurisdiction as
 with c as (
-  select state, sum(pay) as pay 
+  select state, jurisdiction_id, sum(pay) as pay 
   from summary.claims_yearly_state_jurisdiction_2015
   where year>=1994 and year<=2014
-  group by state
-  order by state),
+  group by state, jurisdiction_id
+  order by state, jurisdiction_id),
 ck as (
-  select state, sum(pay) as pay 
+  select state, jurisdiction_id, sum(pay) as pay 
   from summary.claims_yearly_state_jurisdiction_2015
   where year>=1994 and year<=2014 and year!=2005
-  group by state
-  order by state),
+  group by state, jurisdiction_id
+  order by state, jurisdiction_id),
 cs as (
-  select state, sum(pay) as pay 
+  select state, jurisdiction_id, sum(pay) as pay 
   from summary.claims_yearly_state_jurisdiction_2015
   where year>=1994 and year<=2014 and year!=2012
-  group by state
-  order by state),
+  group by state, jurisdiction_id
+  order by state, jurisdiction_id),
 cks as (
-  select state, sum(pay) as pay 
+  select state, jurisdiction_id, sum(pay) as pay 
   from summary.claims_yearly_state_jurisdiction_2015
   where year>=1994 and year<=2014 and year!=2005 and year!=2012
-  group by state
-  order by state),
+  group by state, jurisdiction_id
+  order by state, jurisdiction_id),
 p as (
-  select state, sum(t_premium) as premium 
+  select state, jurisdiction_id, sum(t_premium) as premium 
   from summary.policy_yearly_state_jurisdiction_2015
   where year>=1994 and year<=2014
-  group by state
-  order by state),
+  group by state, jurisdiction_id
+  order by state, jurisdiction_id),
 pk as (
-  select state, sum(t_premium) as premium 
+  select state, jurisdiction_id, sum(t_premium) as premium 
   from summary.policy_yearly_state_jurisdiction_2015
   where year>=1994 and year<=2014 and year!=2005
-  group by state
-  order by state),
+  group by state, jurisdiction_id
+  order by state, jurisdiction_id),
 ps as (
-  select state, sum(t_premium) as premium 
+  select state, jurisdiction_id, sum(t_premium) as premium 
   from summary.policy_yearly_state_jurisdiction_2015
   where year>=1994 and year<=2014 and year!=2012
-  group by state
-  order by state),
+  group by state, jurisdiction_id
+  order by state, jurisdiction_id),
 pks as (
-  select state, sum(t_premium) as premium 
+  select state, jurisdiction_id, sum(t_premium) as premium 
   from summary.policy_yearly_state_jurisdiction_2015
   where year>=1994 and year<=2014 and year!=2005 and year!=2012
-  group by state
-  order by state)
+  group by state, jurisdiction_id
+  order by state, jurisdiction_id)
 select
 COALESCE(p.state, c.state) AS state,
+COALESCE(p.jurisdiction_id, c.jurisdiction_id) AS jurisdiction_id,
 p.premium, 
 c.pay,
 (p.premium/c.pay) as ratio,
@@ -66,41 +67,42 @@ pks.premium as premium_nonks,
 cks.pay as pay_nonks,
 (pks.premium/cks.pay) as ratio_nonks
 from p
-full outer join c using(state)
-full outer join pk using(state)
-full outer join ps using(state)
-full outer join pks using(state)
-full outer join ck using(state)
-full outer join cs using(state)
-full outer join cks using(state)
-order by 1;
+full outer join c using(state, jurisdiction_id)
+full outer join pk using(state, jurisdiction_id)
+full outer join ps using(state, jurisdiction_id)
+full outer join pks using(state, jurisdiction_id)
+full outer join ck using(state, jurisdiction_id)
+full outer join cs using(state, jurisdiction_id)
+full outer join cks using(state, jurisdiction_id)
+order by 1, 2;
 
-alter table us.premium_pay_state
-add primary key(state);
+alter table us.premium_pay_jurisdiction
+add primary key(state, jurisdiction_id);
 
-update us.premium_pay_state
+update us.premium_pay_jurisdiction
 set ratio = 0.000001 where premium is null;
-update us.premium_pay_state
+update us.premium_pay_jurisdiction
 set ratio_nonk = 0.000001 where premium_nonk is null;
-update us.premium_pay_state
+update us.premium_pay_jurisdiction
 set ratio_nons = 0.000001 where premium_nons is null;
-update us.premium_pay_state
+update us.premium_pay_jurisdiction
 set ratio_nonks = 0.000001 where premium_nonks is null;
 
-update us.premium_pay_state
+update us.premium_pay_jurisdiction
 set ratio = 999999 where pay is null;
-update us.premium_pay_state
+update us.premium_pay_jurisdiction
 set ratio_nonk = 999999 where pay_nonk is null;
-update us.premium_pay_state
+update us.premium_pay_jurisdiction
 set ratio_nons = 999999 where pay_nons is null;
-update us.premium_pay_state
+update us.premium_pay_jurisdiction
 set ratio_nonks = 999999 where pay_nonks is null;
 
-drop table summary.policy_claims_yearly_state_2015;
-create table summary.policy_claims_yearly_state_2015 as
+drop table summary.policy_claims_yearly_jurisdiction_2015;
+create table summary.policy_claims_yearly_jurisdiction_2015 as
 with cs as (
   select 
-    state, 
+    state,
+    jurisdiction_id,
     year,
     sum(c.count) as ccount,
     sum(c.pay) as pay,
@@ -111,21 +113,23 @@ with cs as (
     sum(c.pay_cont) as pay_cont,
     sum(c.pay_icc) as pay_icc
   from summary.claims_yearly_state_jurisdiction_2015 c
-  group by 1,2
-  order by 1,2),
+  group by 1,2,3
+  order by 1,2,3),
 ps as (
   select
     state,
+    jurisdiction_id,
     year,
     sum(p.count) as pcount,
     sum(p.t_premium) as premium,
     sum(p.t_cov_bldg) as t_cov_bldg,
     sum(p.t_cov_cont) as t_cov_cont
   from summary.policy_yearly_state_jurisdiction_2015 p
-  group by 1,2
-  order by 1,2)
+  group by 1,2,3
+  order by 1,2,3)
 select 
 COALESCE(cs.state, ps.state) AS state,
+COALESCE(cs.jurisdiction_id, ps.jurisdiction_id) AS jurisdiction_id,
 COALESCE(cs.year, ps.year) AS year,
 cs.ccount,
 cs.pay,
@@ -140,30 +144,32 @@ ps.premium,
 ps.t_cov_bldg,
 ps.t_cov_cont
 from cs
-full outer join ps using(state, year)
-order by 1, 2;
+full outer join ps using(state, jurisdiction_id, year)
+order by 1, 2, 3;
 
-alter table summary.policy_claims_yearly_state_2015 add primary key(state,year);
+alter table summary.policy_claims_yearly_jurisdiction_2015 add primary key(state,jurisdiction,year);
 
-drop table us.premium_pay_state_noworst;
-create table us.premium_pay_state_noworst as
+drop table us.premium_pay_jurisdiction_noworst;
+create table us.premium_pay_jurisdiction_noworst as
 with cs as ( 
   with s as ( 
     with m as (
-      select pc.state, max(pc.pay) as maxpay 
-      from summary.policy_claims_yearly_state_2015 pc
+      select pc.state, pc.jurisdiction_id, max(pc.pay) as maxpay 
+      from summary.policy_claims_yearly_jurisdiction_2015 pc
       where pc.year>=1994 and pc.year<=2014
-      group by state
-      order by state)
+      group by state, jurisdiction_id
+      order by state, jurisdiction_id)
     select 
-    ps.state, 
+    ps.state,
+    ps.jurisdiction_id,
     ps.year
-    from summary.policy_claims_yearly_state_2015 ps
-    join m using (state)
+    from summary.policy_claims_yearly_jurisdiction_2015 ps
+    join m using (state, jurisdiction_id)
     where ps.year>=1994 and ps.year<=2014 and ps.pay=m.maxpay
-    order by 1) 
+    order by 1, 2) 
   select 
-  c.state, 
+  c.state,
+  c.jurisdiction_id,
   sum(c.pay) as pay
   from summary.claims_yearly_state_jurisdiction_2015 c
   join s using (state)
@@ -173,32 +179,35 @@ with cs as (
 ps as ( 
   with s as ( 
     with m as (
-      select pc.state, max(pc.pay) as maxpay 
-      from summary.policy_claims_yearly_state_2015 pc
+      select pc.state, pc.jurisdiction_id, max(pc.pay) as maxpay 
+      from summary.policy_claims_yearly_jurisdiction_2015 pc
       where pc.year>=1994 and pc.year<=2014
-      group by state
-      order by state)
+      group by state, jurisdiction_id
+      order by state, jurisdiction_id)
     select 
-    ps.state, 
+    ps.state,
+    ps.jurisdiction_id,
     ps.year
-    from summary.policy_claims_yearly_state_2015 ps
-    join m using (state)
+    from summary.policy_claims_yearly_jurisdiction_2015 ps
+    join m using (state, jurisdiction_id)
     where ps.year>=1994 and ps.year<=2014 and ps.pay=m.maxpay
-    order by 1) 
+    order by 1, 2) 
   select 
-  p.state, 
+  p.state,
+  p.jurisdiction_id,
   sum(p.t_premium) as premium
   from summary.policy_yearly_state_jurisdiction_2015 p
-  join s using (state)
+  join s using (state, jurisdiction_id)
   where p.year>=1994 and p.year<=2014 and p.year!=s.year
-  group by 1
-  order by 1)
+  group by 1, 2
+  order by 1, 2)
 select 
 COALESCE(cs.state, ps.state) AS state,
+COALLESCE(cs.jurisdiction_id, ps.jurisdiction_id) AS jurisdiction_id,
 ps.premium,
 cs.pay,
 (ps.premium/cs.pay) as ratio
 from ps
-full outer join cs using (state)
-order by 1;
+full outer join cs using (state, jurisdiction_id)
+order by 1, 2;
   
