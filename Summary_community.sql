fe038260699cbb279ecfbb0e561a1bf1963439f7
ruihui0821 <ruihui0821@users.noreply.github.com>
2016-10-20 13:46:27 -0700

    Update Summary_community.sql

--- Summary_community.sql
+++ Summary_community.sql
@@ -1,58 +1,59 @@
 set search_path=summary,fima,public,us,ca;
--- ratios of policy premium to claims payment by jurisdiction
--- according to the re_state and jurisdiction_id column
-drop table us.premium_pay_jurisdiction;
-create table us.premium_pay_jurisdiction as
+
+-- ratios of policy premium to claims payment by community
+-- according to the re_state and community column
+drop table us.premium_pay_community;
+create table us.premium_pay_community as
 with c as (
-  select jurisdiction_id, sum(pay) as pay 
-  from summary.claims_yearly_state_jurisdiction_2015
+  select community, sum(s.pay) as pay 
+  from summary.claims_yearly_state_jurisdiction_2015 s
   where year>=1994 and year<=2014
   group by 1
   order by 1),
 ck as (
-  select jurisdiction_id, sum(pay) as pay 
+  select community, sum(pay) as pay 
   from summary.claims_yearly_state_jurisdiction_2015
   where year>=1994 and year<=2014 and year!=2005
   group by 1
   order by 1),
 cs as (
-  select jurisdiction_id, sum(pay) as pay 
+  select community, sum(pay) as pay 
   from summary.claims_yearly_state_jurisdiction_2015
   where year>=1994 and year<=2014 and year!=2012
   group by 1
   order by 1),
 cks as (
-  select jurisdiction_id, sum(pay) as pay 
+  select community, sum(pay) as pay 
   from summary.claims_yearly_state_jurisdiction_2015
   where year>=1994 and year<=2014 and year!=2005 and year!=2012
   group by 1
   order by 1),
 p as (
-  select jurisdiction_id, sum(t_premium) as premium 
+  select community, sum(t_premium) as premium 
   from summary.policy_yearly_state_jurisdiction_2015
   where year>=1994 and year<=2014
   group by 1
   order by 1),
 pk as (
-  select jurisdiction_id, sum(t_premium) as premium 
+  select community, sum(t_premium) as premium 
   from summary.policy_yearly_state_jurisdiction_2015
   where year>=1994 and year<=2014 and year!=2005
   group by 1
   order by 1),
 ps as (
-  select jurisdiction_id, sum(t_premium) as premium 
+  select community, sum(t_premium) as premium 
   from summary.policy_yearly_state_jurisdiction_2015
   where year>=1994 and year<=2014 and year!=2012
   group by 1
   order by 1),
 pks as (
-  select jurisdiction_id, sum(t_premium) as premium 
+  select community, sum(t_premium) as premium 
   from summary.policy_yearly_state_jurisdiction_2015
   where year>=1994 and year<=2014 and year!=2005 and year!=2012
   group by 1
   order by 1)
 select
-COALESCE(p.jurisdiction_id, c.jurisdiction_id) AS jurisdiction_id,
+COALESCE(p.community, c.community) AS community,
 p.premium, 
 c.pay,
 (p.premium/c.pay) as ratio,
@@ -66,41 +67,41 @@ pks.premium as premium_nonks,
 cks.pay as pay_nonks,
 (pks.premium/cks.pay) as ratio_nonks
 from p
-full outer join c using(jurisdiction_id)
-full outer join pk using(jurisdiction_id)
-full outer join ps using(jurisdiction_id)
-full outer join pks using(jurisdiction_id)
-full outer join ck using(jurisdiction_id)
-full outer join cs using(jurisdiction_id)
-full outer join cks using(jurisdiction_id)
+full outer join c using(community)
+full outer join pk using(community)
+full outer join ps using(community)
+full outer join pks using(community)
+full outer join ck using(community)
+full outer join cs using(community)
+full outer join cks using(community)
 order by 1;
 
-alter table us.premium_pay_jurisdiction
-add primary key(jurisdiction_id);
+alter table us.premium_pay_community
+add primary key(community);
 
-update us.premium_pay_jurisdiction
+update us.premium_pay_community
 set ratio = 0.000001 where premium is null;
-update us.premium_pay_jurisdiction
+update us.premium_pay_community
 set ratio_nonk = 0.000001 where premium_nonk is null;
-update us.premium_pay_jurisdiction
+update us.premium_pay_community
 set ratio_nons = 0.000001 where premium_nons is null;
-update us.premium_pay_jurisdiction
+update us.premium_pay_community
 set ratio_nonks = 0.000001 where premium_nonks is null;
 
-update us.premium_pay_jurisdiction
+update us.premium_pay_community
 set ratio = 999999 where pay is null;
-update us.premium_pay_jurisdiction
+update us.premium_pay_community
 set ratio_nonk = 999999 where pay_nonk is null;
-update us.premium_pay_jurisdiction
+update us.premium_pay_community
 set ratio_nons = 999999 where pay_nons is null;
-update us.premium_pay_jurisdiction
+update us.premium_pay_community
 set ratio_nonks = 999999 where pay_nonks is null;
 
-drop table summary.policy_claims_yearly_jurisdiction_2015;
-create table summary.policy_claims_yearly_jurisdiction_2015 as
+drop table summary.policy_claims_yearly_community_2015;
+create table summary.policy_claims_yearly_community_2015 as
 with cs as (
   select 
-    jurisdiction_id,
+    community,
     year,
     sum(c.count) as ccount,
     sum(c.pay) as pay,
@@ -115,7 +116,7 @@ with cs as (
   order by 1,2),
 ps as (
   select
-    jurisdiction_id,
+    community,
     year,
     sum(p.count) as pcount,
     sum(p.t_premium) as premium,
@@ -125,7 +126,7 @@ ps as (
   group by 1,2
   order by 1,2)
 select 
-COALESCE(cs.jurisdiction_id, ps.jurisdiction_id) AS jurisdiction_id,
+COALESCE(cs.community, ps.community) AS community,
 COALESCE(cs.year, ps.year) AS year,
 cs.ccount,
 cs.pay,
@@ -140,73 +141,73 @@ ps.premium,
 ps.t_cov_bldg,
 ps.t_cov_cont
 from cs
-full outer join ps using(jurisdiction_id, year)
+full outer join ps using(community, year)
 order by 1, 2;
 
-alter table summary.policy_claims_yearly_jurisdiction_2015 add primary key(jurisdiction_id,year);
+alter table summary.policy_claims_yearly_community_2015 add primary key(community,year);
 
-drop table us.premium_pay_jurisdiction_noworst;
-create table us.premium_pay_jurisdiction_noworst as
+drop table us.premium_pay_community_noworst;
+create table us.premium_pay_community_noworst as
 with cs as ( 
   with s as ( 
     with m as (
-      select pc.jurisdiction_id, max(pc.pay) as maxpay 
-      from summary.policy_claims_yearly_jurisdiction_2015 pc
+      select pc.community, max(pc.pay) as maxpay 
+      from summary.policy_claims_yearly_community_2015 pc
       where pc.year>=1994 and pc.year<=2014
       group by 1
       order by 1)
     select 
-    ps.jurisdiction_id,
+    ps.community,
     ps.year
-    from summary.policy_claims_yearly_jurisdiction_2015 ps
-    join m using (jurisdiction_id)
+    from summary.policy_claims_yearly_community_2015 ps
+    join m using (community)
     where ps.year>=1994 and ps.year<=2014 and ps.pay=m.maxpay
     order by 1) 
   select 
-  c.jurisdiction_id,
+  c.community,
   sum(c.pay) as pay
   from summary.claims_yearly_state_jurisdiction_2015 c
-  join s using (jurisdiction_id)
+  join s using (community)
   where c.year>=1994 and c.year<=2014 and c.year!=s.year
   group by 1
   order by 1),
 ps as ( 
   with s as ( 
     with m as (
-      select pc.jurisdiction_id, max(pc.pay) as maxpay 
-      from summary.policy_claims_yearly_jurisdiction_2015 pc
+      select pc.community, max(pc.pay) as maxpay 
+      from summary.policy_claims_yearly_community_2015 pc
       where pc.year>=1994 and pc.year<=2014
       group by 1
       order by 1)
     select 
-    ps.jurisdiction_id,
+    ps.community,
     ps.year
-    from summary.policy_claims_yearly_jurisdiction_2015 ps
-    join m using (jurisdiction_id)
+    from summary.policy_claims_yearly_community_2015 ps
+    join m using (community)
     where ps.year>=1994 and ps.year<=2014 and ps.pay=m.maxpay
     order by 1) 
   select 
-  p.jurisdiction_id,
+  p.community,
   sum(p.t_premium) as premium
   from summary.policy_yearly_state_jurisdiction_2015 p
-  join s using (jurisdiction_id)
+  join s using (community)
   where p.year>=1994 and p.year<=2014 and p.year!=s.year
   group by 1
   order by 1)
 select 
-COALESCE(cs.jurisdiction_id, ps.jurisdiction_id) AS jurisdiction_id,
+COALESCE(cs.community, ps.community) AS community,
 ps.premium,
 cs.pay,
 (ps.premium/cs.pay) as ratio
 from ps
-full outer join cs using (jurisdiction_id)
+full outer join cs using (community)
 order by 1;
 
-drop table us.premium_pay_state_jurisdiction;
-create table us.premium_pay_state_jurisdiction as
+drop table us.premium_pay_state_community;
+create table us.premium_pay_state_community as
 select
 sfc.fipsalphacode as state,
-p.jurisdiction_id,
+p.community,
 p.premium,
 p.pay,
 p.ratio,
@@ -222,8 +223,8 @@ p.ratio_nonks,
 n.premium as premium_noworst,
 n.pay as pay_noworst,
 n.ratio as ratio_noworst
-from us.premium_pay_jurisdiction p
-full outer join us.premium_pay_jurisdiction_noworst n using (jurisdiction_id)
-join fima.jurisdictions j ON (p.jurisdiction_id = j.jurisdiction_id)
+from us.premium_pay_community p
+full outer join us.premium_pay_community_noworst n using (community)
+join fima.jurisdictions j ON (p.community = j.j_cid)
 join fima.statefipscodes sfc ON (sfc.fipsnumbercode = j.j_statefp10)
 order by 1, 2;
