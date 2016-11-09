418b6f608d5715160df1fbb124793d6d545dc7e1
ruihui0821 <ruihui0821@users.noreply.github.com>
2016-11-08 16:13:07 -0800

    Update database_llj.sql

--- database_llj.sql
+++ database_llj.sql
@@ -1,6 +1,67 @@
 set search_path=summary,fima,public,us;
 
-from public.allpolicy a
- join llgridpolicy g using (gis_longi,gis_lati)
- join fima.jurisdictions j using (jurisdiction_id)
- join fima.lljpolicy lj using (jurisdiction_id,llgrid_id)
+-- adding llj_id to policy data
+alter table public.allpolicy add column llj_id integer;
+update public.allpolicy a
+set llj_id = (
+ select lj.llj_id 
+ from fima.lljpolicy lj
+ join llgridpolicy g using (llgrid_id)
+ where a.jurisdiction_id = lj.jurisdiction_id and
+ a.gis_lati = g.gis_lati and
+ a.gis_longi = g.gis_longi limit 1)
+where exists(
+ select lj.llj_id 
+ from fima.lljpolicy lj
+ join llgridpolicy g using (llgrid_id)
+ where a.jurisdiction_id = lj.jurisdiction_id and
+ a.gis_lati = g.gis_lati and
+ a.gis_longi = g.gis_longi limit 1);
+ 
+update public.allpolicy a 
+set llj_id = (
+ select lj.llj_id 
+ from fima.lljpolicy lj
+ where a.jurisdiction_id = lj.jurisdiction_id 
+ order by random() limit 1)
+where llj_id is null and
+exists(
+ select lj.llj_id 
+ from fima.lljpolicy lj
+ join llgridpolicy g using (llgrid_id)
+ where a.jurisdiction_id = lj.jurisdiction_id and
+ a.gis_lati = g.gis_lati and
+ a.gis_longi = g.gis_longi limit 1);
+
+-- adding llj_id to claims data
+alter table public.paidclaims add column llj_id integer;
+update public.paidclaims p
+set llj-id = (
+ select lj.llj_id 
+ from fima.llj lj
+ join llgrid g using (llgrid_id)
+ where p.jurisdiction_id = lj.jurisdiction_id and
+ p.gis_lati = g.gis_lati and
+ p.gis_longi = g.gis_longi limit 1)
+where exists(
+ select lj.llj_id 
+ from fima.llj lj
+ join llgrid g using (llgrid_id)
+ where p.jurisdiction_id = lj.jurisdiction_id and
+ p.gis_lati = g.gis_lati and
+ p.gis_longi = g.gis_longi limit 1);
+ 
+update public.paidclaims p
+set llj_id = (
+ select lj.llj_id 
+ from fima.llj lj
+ where a.jurisdiction_id = lj.jurisdiction_id 
+ order by random() limit 1)
+where llj_id is null and
+exists(
+ select lj.llj_id 
+ from fima.llj lj
+ join llgridpolicy g using (llgrid_id)
+ where a.jurisdiction_id = lj.jurisdiction_id and
+ a.gis_lati = g.gis_lati and
+ a.gis_longi = g.gis_longi limit 1);
