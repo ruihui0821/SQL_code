38984f764157866ead49044875822dd4f481952c
ruihui0821 <ruihui0821@users.noreply.github.com>
2016-11-09 22:23:25 -0800

    Update database_llj.sql

--- database_llj.sql
+++ database_llj.sql
@@ -1,8 +1,8 @@
 set search_path=summary,fima,public,us;
 
--- adding llj_id to policy data
+-- adding llj_id to policy data, 79,882,364
 alter table public.allpolicy add column llj_id integer;
--- updating the llj_id based on jurisdiction_id, gis_lati, gis_longi
+-- updating the llj_id based on jurisdiction_id, gis_lati, gis_longi, 77,483,291
 update public.allpolicy a
 set llj_id = (
  select lj.llj_id 
@@ -19,7 +19,7 @@ where exists(
  a.gis_lati = g.gis_lati and
  a.gis_longi = g.gis_longi);
 
--- updating the rest llj_id based on jurisdiction_id, select a random 
+-- updating the rest llj_id based on jurisdiction_id, select a random , 2,393,782
 update public.allpolicy a 
 set llj_id = (
  select lj.llj_id 
@@ -32,11 +32,11 @@ exists(
  from fima.lljpolicy lj
  where a.jurisdiction_id = lj.jurisdiction_id);
 
--- updating the rest llj_id based on gis_lati, gis_longi, select a random of the already assigned
+-- updating the rest llj_id based on gis_lati, gis_longi, select a random of the already assigned, 4,275
 update public.allpolicy a
 set llj_id = (
  select aa.llj_id 
- from allpolicy aa
+ from public.allpolicy aa
  where a.gis_lati = aa.gis_lati and
  a.gis_longi = aa.gis_longi and
  aa.llj_id is not null
@@ -44,15 +44,15 @@ set llj_id = (
 where llj_id is null and
 exists(
  select aa.llj_id 
- from allpolicy aa
+ from public.allpolicy aa
  where a.gis_lati = aa.gis_lati and
  a.gis_longi = aa.gis_longi and
  aa.llj_id is not null);
 
--- updating the rest llj_id based on re_community, select a random of the already assigned, 
+-- updating the rest llj_id based on re_community, select a random of the already assigned, 805
 update public.allpolicy a
 set llj_id = (
- select pp.llj_id 
+ select aa.llj_id 
  from public.allpolicy aa
  where a.re_community = aa.re_community and
  aa.llj_id is not null
@@ -64,7 +64,7 @@ exists(
  where a.re_community = aa.re_community and
  aa.llj_id is not null);
  
- -- updating the rest llj_id based on re_state, select a random of the already assigned, 
+ -- updating the rest llj_id based on re_state, select a random of the already assigned, 211
 update public.allpolicy a
 set llj_id = (
  select aa.llj_id 
@@ -80,9 +80,9 @@ exists(
  aa.llj_id is not null);
  
 ---------------------------------------------------------------------------------------------------------------------------------------- 
--- adding llj_id to claims data, 1625472
+-- adding llj_id to claims data, 1,625,472
 alter table public.paidclaims add column llj_id integer;
--- updating the llj_id based on jurisdiction_id, gis_lati, gis_longi, 1508065
+-- updating the llj_id based on jurisdiction_id, gis_lati, gis_longi, 1,508,065
 update public.paidclaims p
 set llj_id = (
  select lj.llj_id 
@@ -99,7 +99,7 @@ where exists(
  p.gis_lati = g.gis_lati and
  p.gis_longi = g.gis_longi);
 
--- updating the rest llj_id based on jurisdiction_id, select a random, 115685
+-- updating the rest llj_id based on jurisdiction_id, select a random, 115,685
 update public.paidclaims p
 set llj_id = (
  select lj.llj_id 
@@ -112,7 +112,7 @@ exists(
  from fima.llj lj
  where p.jurisdiction_id = lj.jurisdiction_id);
 
--- updating the rest llj_id based on gis_lati, gis_longi, select a random of the already assigned, 1554
+-- updating the rest llj_id based on gis_lati, gis_longi, select a random of the already assigned, 1,554
 update public.paidclaims p
 set llj_id = (
  select pp.llj_id 
@@ -159,7 +159,15 @@ exists(
  where p.re_state = pp.re_state and
  pp.llj_id is not null);
  
- 
+
+
+
+
+
+
+
+
+
 select p.effdate, corr(p.count, j.income) 
 from summary.policy_monthlyeff_2015_llj p, fima.lljpolicy_income j 
 where p.llj_id = j.llj_id 
