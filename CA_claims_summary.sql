a8fba382ad1876566677a08756f147cd6cffa7aa
ruihui0821 <ruihui0821@users.noreply.github.com>
2016-11-25 14:06:23 -0800

    Update and rename CA_summary.sql to CA_claims_summary.sql

--- CA_claims_summary.sql
+++ CA_claims_summary.sql
@@ -366,74 +366,8 @@ where v.jurisdiction_id in (
  join fima.jurisdictions j using (jurisdiction_id)
  where substr(j.j_cid,1,6) IN ('060044','060045','060046','060047','060048','060049','060050','060051','060052','060053','060054','060055','060075','060076','060077','060078','060079','060080','060081','060082','060084','060085','060086','060088','060089','060170','060172','060188','060189','060191','060299','060300','060302','060303','060384','060385','060387','060388','060389','060390','060391','060392','060393','060403','060404','060405','060406','060407','060409','060440','060443','060450','060454','060457','060644','060663','060706','060738','060747','065029','065063','065065','065066','065071','065073')
  group by 1);
- 
--- Policy Data
--- 4.
-drop table ca.ca_policy_monthly_summary_j;
-create table ca.ca_policy_monthly_summary_j as
-select
-jurisdiction_id,
-extract(year from end_eff_dt) as year,
-extract(month from end_eff_dt) as month,
-sum(condo_unit) as condo_count,
-sum(t_premium) as t_premium,
-sum(t_cov_bldg) as t_cov_bldg,
-sum(t_cov_cont) as t_cov_cont
-from ca.caallpolicy
-group by 1,2,3
-order by 1,2,3;
 
-alter table ca.ca_policy_monthly_summary_j
-add primary key (jurisdiction_id,year,month);
 
---4.1
-drop table ca.ca_policy_monthly_summary_2015_j;
-create table ca.ca_policy_monthly_summary_2015_j as
-with s as (
- select
- jurisdiction_id,
- year,
- month,
- condo_count,
- t_premium,
- t_cov_bldg,
- t_cov_cont
- from ca.ca_policy_monthly_summary_j)
-select
-s.jurisdiction_id,
-year,
-month,
-condo_count,
-t_premium*rate as t_premium,
-t_cov_bldg*rate as t_cov_bldg,
-t_cov_cont*rate as t_cov_cont,
-to_year as dollars_in
-from s join inflation i on (i.from_year=s.year)
-where i.to_year=2015;
-
-alter table ca.ca_policy_monthly_summary_2015_j add primary key (jurisdiction_id,year,month);
-
--- 5. Sacramento Valley, San Joaquin Valley and the rest of california
-alter table ca.ca_policy_monthly_summary_2015_j add column valley character varying(6);
-
-update ca.ca_policy_monthly_summary_2015_j v
-set valley = 'SAV'
-where v.jurisdiction_id in (
- select j.jurisdiction_id
- from ca.ca_policy_monthly_summary_2015_j
- join fima.jurisdictions j using (jurisdiction_id)
- where substr(j.j_cid,1,6) IN ( '060017','060019','060020','060022','060023','060024','060057','060059','060239','060240','060241','060242','060243','060262','060263','060264','060265','060266','060358','060359','060360','060394','060395','060396','060398','060400','060423','060424','060425','060426','060427','060428','060437','060460','060650','060721','060728','060746','060748','060758','060765','060767','060772','065053','065064')
- group by 1);
-
-update ca.ca_policy_monthly_summary_2015_j v
-set valley = 'SJV'
-where v.jurisdiction_id in (
- select j.jurisdiction_id
- from ca.ca_policy_monthly_summary_2015_j
- join fima.jurisdictions j using (jurisdiction_id)
- where substr(j.j_cid,1,6) IN ('060044','060045','060046','060047','060048','060049','060050','060051','060052','060053','060054','060055','060075','060076','060077','060078','060079','060080','060081','060082','060084','060085','060086','060088','060089','060170','060172','060188','060189','060191','060299','060300','060302','060303','060384','060385','060387','060388','060389','060390','060391','060392','060393','060403','060404','060405','060406','060407','060409','060440','060443','060450','060454','060457','060644','060663','060706','060738','060747','065029','065063','065065','065066','065071','065073')
- group by 1);
- 
 -- Data Summary by flood zone
 alter table ca.capaidclaims add column fzone character varying(3);
 
@@ -458,21 +392,23 @@ set fzone = 'D'
 where substr(flood_zone,1,1) IN ('D');
 
 with s as (
- select extract(year from dt_of_loss) as year, count(*), sum(pay_cont) as pay
+ select extract(year from dt_of_loss) as year, count(*), sum(pay_cont + pay_bldg) as pay
  from ca.capaidclaims c
  where fzone = 'A'
  group by 1
  order by 1)
 select * from s order by year;
 
-select sum(condo_unit) 
-from ca.caallpolicy p
-where end_eff_dt >= '1994-01-01' and end_eff_dt <= '2014-12-31';
-
-select count(*)
-from ca.capaidclaims c
-where dt_of_loss >= '1994-01-01' and dt_of_loss <= '2014-12-31'
-and substr(flood_zone,1,1) IN ('A');
+with s as (
+ select extract(year from dt_of_loss) as year, 
+        count(*), 
+        sum(pay_cont) as pay_cont, 
+        sum(pay_bldg) as pay_bldg, 
+        sum(pay_cont + pay_bldg) as pay
+ from ca.capaidclaims c
+ group by 1
+ order by 1)
+select * from s order by year;
 
 -- Data summary by Valley
 select 
@@ -482,17 +418,13 @@ from ca.ca_claims_monthly_summary_2015_j c
 group by year
 order by year;
 
-select sum(t_premium) 
-from ca.ca_policy_monthly_summary_2015_j p
-where p.year>=1994 and p.year<= 2014 and valley = 'SAV'
-group by year
-order by year;
-
 select flood_zone, sum(count) as count, sum(pay) 
 from ca.ca_claims_monthly_summary_2015_j c
 where c.year>=1994 and c.year<= 2014
 group by 1
 order by 1;
+
+
 
 -- Sacramento Valley Communities
 substr(community,1,6) IN ( '060017','060019','060020','060022','060023','060024','060057','060059','060239','060240','060241','060242','060243','060262','060263','060264','060265','060266','060358','060359','060360','060394','060395','060396','060398','060400','060423','060424','060425','060426','060427','060428','060437','060460','060650','060721','060728','060746','060748','060758','060765','060767','060772','065053','065064')
