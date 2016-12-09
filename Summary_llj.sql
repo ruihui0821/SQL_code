d0088d43869d3b8d32b2fb1d6ba893f94d6df7e7
ruihui0821 <ruihui0821@users.noreply.github.com>
2016-12-09 13:38:19 -0800

    Update Summary_llj.sql

--- Summary_llj.sql
+++ Summary_llj.sql
@@ -276,6 +276,98 @@ from p, c, ps, cs;
  65097524.2441 | 91501295.565603 | 0.711438279006963 |   63056524.8649 | 74064621.951075 | 0.851371723824546
  
 -------------------------------------------------------------------------------------------------------------------------------------
+-- 4. lowmedincome summary
+drop table summary.policy_claims_yearly_2015_lowmedincome;
+create table summary.policy_claims_yearly_2015_lowmedincome as
+with cs as (
+  select 
+    c.year,
+    sum(c.count) as ccount,
+    sum(c.pay_bldg + c.pay_cont) as pay,
+    sum(c.pay_bldg) as pay_bldg,
+    sum(c.pay_cont) as pay_cont,
+    sum(c.t_dmg_bldg) as t_dmg_bldg,
+    sum(c.t_dmg_cont) as t_dmg_cont,
+    sum(c.t_dmg_bldg + c.t_dmg_cont) as t_dmg
+  from summary.claims_yearly_2015_llj c, fima.llj_income li
+  where c.llj_id = li.llj_id and li.lowmedincome = 'Y' 
+  group by 1
+  order by 1),
+ps as (
+  select
+    p.year,
+    sum(p.count) as pcount,
+    sum(p.t_premium) as premium,
+    sum(p.t_cov_bldg) as t_cov_bldg,
+    sum(p.t_cov_cont) as t_cov_cont
+  from summary.policy_yearly_2015_llj p, fima.lljpolicy_income lpi
+  where p.llj_id = lpi.llj_id and lpi.lowmedincome = 'Y' 
+  group by 1
+  order by 1)
+select 
+COALESCE(cs.year, ps.year) AS year,
+cs.ccount,
+cs.pay,
+cs.pay_bldg,
+cs.pay_cont,
+cs.t_dmg_bldg,
+cs.t_dmg_cont,
+cs.t_dmg,
+ps.pcount,
+ps.premium,
+ps.t_cov_bldg,
+ps.t_cov_cont
+from cs
+full outer join ps using(year)
+order by 1;
+
+alter table summary.policy_claims_yearly_2015_lowmedincome add primary key(year);
+
+with c as (
+  select sum(pay) as pay 
+  from summary.policy_claims_yearly_2015_lowmedincome
+  where year>=1994 and year<=2014),
+cs as ( 
+  select
+  sum(pc.pay) as pay
+  from summary.policy_claims_yearly_2015_lowmedincome pc
+  where pc.year>=1994 and 
+        pc.year<=2014 and 
+        pc.year not in ( 
+        select 
+        pcc.year
+        from summary.policy_claims_yearly_2015_lowmedincome pcc
+        where pcc.year>=1994 and pcc.year<=2014 order by pcc.pay desc limit 1) ),  
+p as (
+  select sum(premium) as premium 
+  from summary.policy_claims_yearly_2015_lowmedincome
+  where year>=1994 and year<=2014),
+ps as (
+  select 
+  sum(pc.premium) as premium
+  from summary.policy_claims_yearly_2015_lowmedincome pc
+  where pc.year>=1994 and 
+        pc.year<=2014 and 
+        pc.year not in ( 
+        select 
+        pcc.year
+        from summary.policy_claims_yearly_2015_lowmedincome pcc
+        where pcc.year>=1994 and pcc.year<=2014 order by pcc.pay desc limit 1) )
+select
+p.premium, 
+c.pay,
+(p.premium/c.pay) as ratio,
+ps.premium as premium_noworst,
+cs.pay as pay_noworst,
+(ps.premium/cs.pay) as ratio_noworst
+from p, c, ps, cs;
+
+    ppremium      |       pay        |       ratio       | premium_noworst  |   pay_noworst    |  ratio_noworst   
+------------------+------------------+-------------------+------------------+------------------+------------------
+ 48096093412.1524 | 50327389824.8206 | 0.955664372413612 | 45879263449.0459 | 29688496900.5498 | 1.54535487608995
+
+2005 is the year with the largest payment
+-------------------------------------------------------------------------------------------------------------------------------------
 -- tables for making videos for poverty group
 -- not making yet
 drop table us.policy_monthlyeff_2015_llj_pop10_poverty;
