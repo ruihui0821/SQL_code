f7cf5757243cb909f588a1d976a254aaf53d2cc1
ruihui0821 <ruihui0821@users.noreply.github.com>
2016-12-10 16:06:37 -0800

    Update community_income.sql

--- community_income.sql
+++ community_income.sql
@@ -1,21 +1,92 @@
 drop table summary.jurisdiction_income;
+
 create table summary.jurisdiction_income as
 with s as (
-select 
-p.jurisdiction_id,
-p.pcount,
-p.premium
-from summary.policy_claims_yearly_jurisdiction_2015 p
-where year = 2014)
+  select 
+  p.jurisdiction_id,
+  p.pcount,
+  p.premium
+  from summary.policy_claims_yearly_jurisdiction_2015 p
+  where p.year = 2014
+  order by 1),
+ss as (
+  select
+  p.jurisdiction_id,
+  sum(p.ccount) as ccount,
+  sum(p.pay) as pay
+  from summary.policy_claims_yearly_jurisdiction_2015 p
+  group by 1
+  order by 1)
 select j.jurisdiction_id,
 j.income,
-sum(pc.ccount) as ccount,
-sum(pc.pay) as pay,
 s.pcount as pcount2014,
 s.premium as premium2014,
+ss.ccount,
+ss.pay,
 jj.j_pop10,
 jj.boundary
 from fima.j_income j
-join fima.jurisdictions jj using(jurisdiction_id)
-join summary.policy_claims_yearly_jurisdiction_2015 pc using(jurisdiction_id)
-join s using(jurisdiciton_id);
+full outer join fima.jurisdictions jj using(jurisdiction_id)
+full outer join s using(jurisdiction_id)
+full outer join ss using(jurisdiction_id);
+
+alter table summary.jurisdiction_income add primary key(jurisdiction_id);
+
+alter table fima.statefipscodes add column lowmiddleincome double precision;
+
+update fima.statefipscodes s
+set lowmiddleincome = (
+  select 1.2*ss.mhincome
+  from fima.statefipscodes ss
+  where s.fipsnumbercode = ss.fipsnumbercode);
+
+
+alter table summary.jurisdiction_income add column lowincome character varying (1);
+alter table summary.jurisdiction_income add column lowmiddleincome character varying (1);
+
+update summary.jurisdiction_income ji
+set lowincome = 'Y' 
+where ji.income <= (
+  select s.lowincome 
+  from fima.statefipscodes s, fima.jurisdictions j
+  where ji.jurisdiction_id = j.jurisdiction_id and
+  j.j_statefp10 = s.fipsnumbercode);
+  
+update summary.jurisdiction_income ji
+set lowincome = 'N' 
+where ji.income > (
+  select s.lowincome 
+  from fima.statefipscodes s, fima.jurisdictions j
+  where ji.jurisdiction_id = j.jurisdiction_id and
+  j.j_statefp10 = s.fipsnumbercode);
+
+select lowincome, count(*), count(*)/28061.0*100 as percent from summary.jurisdiction_income group by 1;
+
+ lowincome | count |         percent         
+-----------+-------+-------------------------
+ Y         |  5958 | 21.23231531306795908900
+ N         | 22103 | 78.76768468693204091100
+ 
+update summary.jurisdiction_income ji
+set lowmiddleincome = 'Y' 
+where ji.income <= (
+  select s.lowmiddleincome 
+  from fima.statefipscodes s, fima.jurisdictions j
+  where ji.jurisdiction_id = j.jurisdiction_id and
+  j.j_statefp10 = s.fipsnumbercode);
+  
+update summary.jurisdiction_income ji
+set lowmiddleincome = 'N' 
+where ji.income > (
+  select s.lowmiddleincome
+  from fima.statefipscodes s, fima.jurisdictions j
+  where ji.jurisdiction_id = j.jurisdiction_id and
+  j.j_statefp10 = s.fipsnumbercode);
+
+select lowmiddleincome, count(*), count(*)/28061.0*100 as percent from summary.jurisdiction_income group by 1;
+ 
+ lowmiddleincome | count |         percent         
+-----------------+-------+-------------------------
+ Y               | 23303 | 83.04408253447845764600
+ N               |  4758 | 16.95591746552154235400
+
