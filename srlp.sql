20cdda9dd8ffab47fd43739b3c32ef5aab1e293d
ruihui0821 <ruihui0821@users.noreply.github.com>
2017-01-10 11:34:56 -0800

    Update srlp.sql

--- srlp.sql
+++ srlp.sql
@@ -12,3 +12,25 @@ select commname, commno, count(*) from srlp where commno not in (select j.j_cid 
  FAYETTE CO. W.C.& I.D.-MONUMENT HILL        | 481565 |     1
  FAYETTE COUNTY*                             | 210066 |     1
 (10 rows)
+
+12:Florida
+125138#, PENSACOLA BEACH-SANTA ROSA ISLAND AUTHORITY, ESCAMBIA COUNTY
+
+34: New Jersey
+340570#, NEW JERSEY MEADOWLANDS COMMISSION,  BERGEN COUNTY
+The Hackensack Meadowlands
+Commission was renamed the New
+Jersey Meadowlands Commission on
+August 27, 2001.
+36: New York
+361591#, SENECA NATION OF INDIANS CATTARAUGUS, COUNTY/ERIE, COUNTY/CHAUTAUQUA, COUNTY/ALLEGANY COUNTY
+05: ARKANSAS
+050168#, HELENA-WEST HELENA, CITY OF Helena and West Helena have consolidated into a single government. The name of the new community is "Helena-West Helena". Use CID 050168 for all policies in the former communities of "Helena" (CID 050168) and "West Helena" (CID 050171). The Initia, 
+PHILLIPS COUNTY
+13: Geogia
+135175#, BROOKHAVEN, CITY OF BROOKHAVEN IS LOCATED ON DEKALB COUNTY FIRM PANELS: 0011J, 0012J, 0013J, 0014J, AND 0016J DATED 05/16/2013. The inital FIRM date for Brookhaven is 05/15/1980.
+DEKALB COUNTY
+48: Texas
+481565#, FAYETTE CO. W.C.& I.D.-MONUMENT HILL, FAYETTE COUNTY
+
+125098, 481537, 130159, 210066: not available in the national community data.
