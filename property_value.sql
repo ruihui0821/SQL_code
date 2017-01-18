0ef2c8523c904765d09eaeedd478e3a635bf0ed6
ruihui0821 <ruihui0821@users.noreply.github.com>
2017-01-17 20:10:21 -0800

    Update property_value.sql

--- property_value.sql
+++ property_value.sql
@@ -24,61 +24,105 @@ where exists(
   from public.claims c
   where p.gid = c.gid);
   
-drop table us.waterdepth_damage_value;
-create table us.waterdepth_damage_value as
-select
+drop table us.waterdepth_damage_value_2015;
+create table us.waterdepth_damage_value_2015 as
+with s as (
+ select
   gid,
+  extract(year from dt_of_loss) as year,
   waterdepth, 
+  pay_bldg,
+  pay_cont,
   pay_bldg + pay_cont as pay, 
+  t_dmg_bldg,
+  t_dmg_cont,
   t_dmg_bldg + t_dmg_cont as dmg,
+  t_prop_val,
+  val_cont,
   t_prop_val + val_cont as value,
   (pay_bldg + pay_cont)/(t_prop_val + val_cont) as paypercent,
   (t_dmg_bldg + t_dmg_cont)/(t_prop_val + val_cont) as dmgpercent
 from public.paidclaims
-where t_prop_val+val_cont > 0;
+where t_prop_val+val_cont > 0 and t_dmg_bldg + t_dmg_cont > 0)
+select
+  gid,
+  waterdepth, 
+  pay_bldg*rate as pay_bldg,
+  pay_cont*rate as pay_cont,
+  pay*rate as pay, 
+  t_dmg_bldg*rate as t_dmg_bldg,
+  t_dmg_cont*rate as t_dmg_cont,
+  dmg*rate as dmg,
+  t_prop_val*rate as t_prop_val,
+  val_cont*rate as val_cont,
+  value*rate as value,
+  paypercent,
+  dmgpercent
+from s join public.inflation i on (i.from_year=s.year)
+where i.to_year=2015;
 
   count  
 ---------
- 1587995
+ 1584036
  
-alter table us.waterdepth_damage_value add primary key (gid);
+alter table us.waterdepth_damage_value_2015 add primary key (gid);
 
-select corr(paypercent, waterdepth), corr(dmgpercent, waterdepth) from us.waterdepth_damage_value;
-         corr          |         corr          
------------------------+-----------------------
- 3.26312559673119e-005 | 9.79666446154346e-006
+select
+waterdepth,
+count(*),
+sum(pay) as tpay,
+sum(dmg) as tdmg,
+sum(value) as tvalue
+from us.waterdepth_damage_value_2015
+group by 1
+order by 1;
+
+
+select count(*), count(*)/1584036.0 from us.waterdepth_damage_value_2015 where waterdepth >=-1 and waterdepth <= 1;
+ 836131 | 0.52784848324154249020
+
+select count(*), count(*)/1584036.0 from us.waterdepth_damage_value_2015 where waterdepth >=-3 and waterdepth <= 3;
+ 1080815 | 0.68231719481122903772
+ 
+select count(*), count(*)/1584036.0 from us.waterdepth_damage_value_2015 where waterdepth >=-10 and waterdepth <= 10;
+ 1322734 | 0.83504036524422424743
+
+select count(*), count(*)/1584036.0 from us.waterdepth_damage_value_2015 where waterdepth >=-20 and waterdepth <= 20;
+ 1385111 | 0.87441888946968376981
+
+select count(*), count(*)/1584036.0 from us.waterdepth_damage_value_2015 where waterdepth >=-30 and waterdepth <= 30;
+ 1411779 | 0.89125436543108868738
+
+select count(*), count(*)/1584036.0 from us.waterdepth_damage_value_2015 where waterdepth >=-10 and waterdepth <= 30;
+ 1410893 | 0.89069503470880712307
+
+select count(*), count(*)/1584036.0 from us.waterdepth_damage_value_2015 where waterdepth >=0 and waterdepth <= 30;
+ 1330693 | 0.84006487226300412364
+
+
+
+select corr(paypercent, waterdepth), corr(dmgpercent, waterdepth) from us.waterdepth_damage_value_2015;
+ 5.52721707303675e-005 | 1.03955922401425e-005
 
 -- paypercent
-select count(*), count(*)/1587995.0, corr(paypercent, waterdepth) from us.waterdepth_damage_value where dmgpercent <=10;
-  count  |        ?column?        |         corr         
----------+------------------------+----------------------
- 1587516 | 0.99969836177066048697 | 0.000298499128653996
+select count(*), count(*)/1584036.0, corr(paypercent, waterdepth) from us.waterdepth_damage_value_2015 where dmgpercent <=10;
+ 1583557 | 0.99969760788264913171 | 0.00126842930147658
 
-select count(*), count(*)/1587995.0, corr(paypercent, waterdepth) from us.waterdepth_damage_value where dmgpercent <=2;
-  count  |        ?column?        |         corr         
----------+------------------------+----------------------
- 1584996 | 0.99811145501087849773 | 0.000283937624060079
+select count(*), count(*)/1584036.0, corr(paypercent, waterdepth) from us.waterdepth_damage_value_2015 where dmgpercent <=2;
+ 1581037 | 0.99810673494794310230 | 0.00123617649630611
 
-select count(*), count(*)/1587995.0, corr(paypercent, waterdepth) from us.waterdepth_damage_value where dmgpercent <=1;
-  count  |        ?column?        |          corr          
----------+------------------------+------------------------
- 1487206 | 0.93653065658267185980 | -3.87183610401184e-005
+select count(*), count(*)/1584036.0, corr(paypercent, waterdepth) from us.waterdepth_damage_value_2015 where dmgpercent <=1;
+ 1483247 | 0.93637202689837857220 | 0.000511352900448155
 
 -- dmgpercent
-select count(*), count(*)/1587995.0, corr(dmgpercent, waterdepth) from us.waterdepth_damage_value where dmgpercent <=10;
-  count  |        ?column?        |       corr        
----------+------------------------+-------------------
- 1587516 | 0.99969836177066048697 | 0.095958648136763
+select count(*), count(*)/1584036.0, corr(dmgpercent, waterdepth) from us.waterdepth_damage_value_2015 where dmgpercent <=10;
+ 1583557 | 0.99969760788264913171 | 0.0963274975196068
 
-select count(*), count(*)/1587995.0, corr(dmgpercent, waterdepth)  from us.waterdepth_damage_value where dmgpercent <=2;
-  count  |        ?column?        |       corr       
----------+------------------------+------------------
- 1584996 | 0.99811145501087849773 | 0.10012113173859
+select count(*), count(*)/1584036.0, corr(dmgpercent, waterdepth)  from us.waterdepth_damage_value_2015 where dmgpercent <=2;
+ 1581037 | 0.99810673494794310230 | 0.100515307073933
 
-select count(*), count(*)/1587995.0, corr(dmgpercent, waterdepth) from us.waterdepth_damage_value where dmgpercent <=1;
-  count  |        ?column?        |       corr        
----------+------------------------+-------------------
- 1487206 | 0.93653065658267185980 | 0.075152285329157
+select count(*), count(*)/1584036.0, corr(dmgpercent, waterdepth) from us.waterdepth_damage_value_2015 where dmgpercent <=1;
+ 1483247 | 0.93637202689837857220 | 0.0755997320264223
  
 
 -- California
