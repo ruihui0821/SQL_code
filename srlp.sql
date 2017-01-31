75abf17d2830c7e588b4319a8b6604d1c89c464d
ruihui0821 <ruihui0821@users.noreply.github.com>
2017-01-31 10:53:02 -0800

    Update srlp.sql

--- srlp.sql
+++ srlp.sql
@@ -15,22 +15,36 @@ select commname, commno, count(*) from srlp where commno not in (select j.j_cid 
 
 12:Florida
 125138#, PENSACOLA BEACH-SANTA ROSA ISLAND AUTHORITY, ESCAMBIA COUNTY
+-- change to 120082 PENSACOLA, CITY OF ESCAMBIA COUNTY
 
 34: New Jersey
 340570#, NEW JERSEY MEADOWLANDS COMMISSION,  BERGEN COUNTY
-The Hackensack Meadowlands
-Commission was renamed the New
-Jersey Meadowlands Commission on
-August 27, 2001.
+The Hackensack Meadowlands Commission was renamed the New Jersey Meadowlands Commission on August 27, 2001.
+-- change to 340039, HACKENSACK, CITY OF BERGEN COUNTY
 36: New York
-361591#, SENECA NATION OF INDIANS CATTARAUGUS, COUNTY/ERIE, COUNTY/CHAUTAUQUA, COUNTY/ALLEGANY COUNTY
+361591#, SENECA NATION OF INDIANS CATTARAUGUS COUNTY/ERIE COUNTY/CHAUTAUQUA COUNTY/ALLEGANY COUNTY
+-- change to 361367 CATTARAUGUS, VILLAGE OF CATTARAUGUS COUNTY
 05: ARKANSAS
+050171
 050168#, HELENA-WEST HELENA, CITY OF Helena and West Helena have consolidated into a single government. The name of the new community is "Helena-West Helena". Use CID 050168 for all policies in the former communities of "Helena" (CID 050168) and "West Helena" (CID 050171). The Initia, 
 PHILLIPS COUNTY
+-- change to 050168
 13: Geogia
 135175#, BROOKHAVEN, CITY OF BROOKHAVEN IS LOCATED ON DEKALB COUNTY FIRM PANELS: 0011J, 0012J, 0013J, 0014J, AND 0016J DATED 05/16/2013. The inital FIRM date for Brookhaven is 05/15/1980.
 DEKALB COUNTY
+-- change to 130065, 130065# DEKALB COUNTY * DEKALB COUNTY
 48: Texas
 481565#, FAYETTE CO. W.C.& I.D.-MONUMENT HILL, FAYETTE COUNTY
+-- change to 480815, 480815# FAYETTE COUNTY* FAYETTE COUNTY
 
-125098, 481537, 130159, 210066: not available in the national community data.
+130159
+-- change to 130158, 130158# AUGUSTA-RICHMOND COUNTY, CITY OF, RICHMOND COUNTY, The City of Augusta and Richmond County Consolidated into a new community The City of Augusta- Richmond County.
+
+210066
+-- change to 210067, 210067# LEXINGTON-FAYETTE URBAN COUNTY GOVERNMENT, FAYETTE COUNTY
+
+125098
+-- change to 120635, 120635# MIAMI-DADE COUNTY*, MIAMI-DADE COUNTY, INCLUDES THE UNINCORPORATED AREAS ONLY
+
+481537
+-- change to 480484# CONROE, CITY OF, MONTGOMERY COUNTY
