9c75cd5bea09de1f4f8ded2abf6e612ee922764a
ruihui0821 <ruihui0821@users.noreply.github.com>
2017-02-09 19:33:38 -0800

    Update flood_half_memory.sql

--- flood_half_memory.sql
+++ flood_half_memory.sql
@@ -92,9 +92,50 @@ order by 1;
 
 
 
+ALTERNATIVE, seperate the three tables:
+drop table summary.policy_dailyeff_2015_s;
+create table summary.policy_dailyeff_2015_s AS
+  SELECT 
+  b.cid, 
+  SUM(b.count) AS scount, 
+  SUM(b.t_premium) AS spremium,
+  SUM(b.t_cov_bldg) AS st_cov_bldg,
+  SUM(b.t_cov_cont) AS st_cov_cont,
+  d.as_of_date AS sdate 
+  FROM GENERATE_SERIES('1994-01-01'::timestamp, '2015-01-01'::timestamp, interval '1 day') d (as_of_date)
+  LEFT JOIN summary.policy_dailynew_2015 b ON b.end_eff_dt < d.as_of_date
+  GROUP BY b.cid, d.as_of_date
+  ORDER BY b.cid, d.as_of_date;
+  
+drop table summary.policy_dailyeff_2015_e;
+create table summary.policy_dailyeff_2015_e AS
+  SELECT 
+  b.cid, 
+  SUM(b.count) AS ecount,
+  SUM(b.t_premium) AS epremium,
+  SUM(b.t_cov_bldg) AS et_cov_bldg,
+  SUM(b.t_cov_cont) AS et_cov_cont,
+  d.as_of_date + interval '1 year' as edate 
+  FROM GENERATE_SERIES('1993-01-01'::timestamp, '2014-01-01'::timestamp, interval '1 day') d (as_of_date)
+  LEFT JOIN summary.policy_dailynew_2015 b ON b.end_eff_dt < d.as_of_date
+  GROUP BY b.cid, d.as_of_date
+  ORDER BY b.cid, d.as_of_date;
+  
+drop table summary.policy_dailyeff_2015;
+create table summary.policy_dailyeff_2015 as 
+select
+s.cid,
+s.sdate- interval '1 day' as effdate,
+s.scount - e.ecount as count,
+s.spremium - e.epremium as premium,
+s.st_cov_bldg - e.et_cov_bldg as t_cov_bldg,
+s.st_cov_cont - e.et_cov_cont as t_cov_cont
+from summary.policy_dailyeff_2015_s s
+join summary.policy_dailyeff_2015_e e on (s.cid = e.cid) and (s.sdate = e.edate)
+order by s.cid, s.sdate;
 
-
-
+----------------------------------------------------------------------------------------------------------------------------------------
+-- county summary
 -- California
 WITH s AS (
   SELECT 
@@ -173,7 +214,7 @@ WITH s AS (
   d.as_of_date AS sdate 
   FROM GENERATE_SERIES('1994-01-01'::timestamp, '2015-01-01'::timestamp, interval '1 day') d (as_of_date)
   LEFT JOIN summary.policy_dailynew_2015 b ON b.end_eff_dt < d.as_of_date
-  where b.cid in (select n.cid from fima.nation n where county = 'ALBANY COUNTY' and statefp = '36')
+  where b.cid in (select n.cid from fima.nation n where county in ('ERIE COUNTY', 'ERIE COUNTY/CATTARAUGUS COUNTY') and statefp = '36')
   GROUP BY b.cid, d.as_of_date
   ORDER BY b.cid, d.as_of_date),
 e AS (
@@ -184,7 +225,41 @@ e AS (
   d.as_of_date + interval '1 year' as edate 
   FROM GENERATE_SERIES('1993-01-01'::timestamp, '2014-01-01'::timestamp, interval '1 day') d (as_of_date)
   LEFT JOIN summary.policy_dailynew_2015 b ON b.end_eff_dt < d.as_of_date
-  where b.cid in (select n.cid from fima.nation n where county = 'ALBANY COUNTY' and statefp = '36')
+  where b.cid in (select n.cid from fima.nation n where county in ('ERIE COUNTY', 'ERIE COUNTY/CATTARAUGUS COUNTY') and statefp = '36')
+  GROUP BY b.cid, d.as_of_date
+  ORDER BY b.cid, d.as_of_date)
+select
+s.sdate- interval '1 day' as effdate,
+sum(s.scount) - sum(e.ecount) as count,
+sum(s.spremium) - sum(e.epremium) as premium
+from s 
+join e on (s.cid = e.cid) and (s.sdate = e.edate)
+group by s.sdate
+order by s.sdate;
+
+-- Illinois
+drop table ca.fmhl_county;
+create table ca.fmhl_county as
+WITH s AS (
+  SELECT 
+  b.cid, 
+  SUM(b.count) AS scount, 
+  SUM(b.t_premium) AS spremium,
+  d.as_of_date AS sdate 
+  FROM GENERATE_SERIES('1994-01-01'::timestamp, '2015-01-01'::timestamp, interval '1 day') d (as_of_date)
+  LEFT JOIN summary.policy_dailynew_2015 b ON b.end_eff_dt < d.as_of_date
+  where b.cid in (select n.cid from fima.nation n where county ='ERIE COUNTY' and statefp = '17')
+  GROUP BY b.cid, d.as_of_date
+  ORDER BY b.cid, d.as_of_date),
+e AS (
+  SELECT 
+  b.cid, 
+  SUM(b.count) AS ecount,
+  SUM(b.t_premium) AS epremium,
+  d.as_of_date + interval '1 year' as edate 
+  FROM GENERATE_SERIES('1993-01-01'::timestamp, '2014-01-01'::timestamp, interval '1 day') d (as_of_date)
+  LEFT JOIN summary.policy_dailynew_2015 b ON b.end_eff_dt < d.as_of_date
+  where b.cid in (select n.cid from fima.nation n where county ='ERIE COUNTY' and statefp = '17')
   GROUP BY b.cid, d.as_of_date
   ORDER BY b.cid, d.as_of_date)
 select
