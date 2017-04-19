fa128829125ff1dee60d3b68f167778f208f877b
ruihui0821 <ruihui0821@users.noreply.github.com>
2017-04-18 18:10:37 -0700

    Update flood_half_memory3.sql

--- flood_half_memory3.sql
+++ flood_half_memory3.sql
@@ -36,3 +36,93 @@ group by 1, 2
 order by 1, 2;
 
 alter table summary.fmhl_crs add primary key (state, county);
+
+---------------------------------------------------------------------------------------------------------------------------------------
+-- not necessary
+alter table summary.fmhl add column aland double precision;
+alter table summary.fmhl add column awater double precision;
+
+update summary.fmhl f
+set aland = (
+  select u.aland
+  from fima.us_county_2015 u
+  join fima.statefipscodes s on (u.statefp = s.fipsnumbercode)
+  where s.fipsalphacode = f.state
+  and u.name = f.county)
+where exists(
+  select u.aland
+  from fima.us_county_2015 u
+  join fima.statefipscodes s on (u.statefp = s.fipsnumbercode)
+  where s.fipsalphacode = f.state
+  and u.name = f.county);
+
+---------------------------------------------------------------------------------------------------------------------------------------
+
+update summary.fmhl f
+set awater = (
+  select u.awater
+  from fima.us_county_2015 u
+  join fima.statefipscodes s on (u.statefp = s.fipsnumbercode)
+  where s.fipsalphacode = f.state
+  and u.name = f.county)
+where exists(
+  select u.awater
+  from fima.us_county_2015 u
+  join fima.statefipscodes s on (u.statefp = s.fipsnumbercode)
+  where s.fipsalphacode = f.state
+  and u.name = f.county);
+
+alter table fima.us_county_2015 add column state character varying(2);
+update fima.us_county_2015 u
+set state = (
+  select s.fipsalphacode
+  from fima.statefipscodes s
+  where u.statefp = s.fipsnumbercode)
+where exists(
+  select s.fipsalphacode
+  from fima.statefipscodes s
+  where u.statefp = s.fipsnumbercode);
+  
+drop table summary.fmhl_urban;
+create table summary.fmhl_urban as
+with s as (
+  select
+  c.state,
+  c.name as county,
+  u.id,
+  ST_Area(ST_Intersection(c.geom, u.geom)) as urban
+  from fima.us_county_2015 c, fima.us_urban_2010 u
+  where ST_Intersects(c.geom::geometry,u.geom::geometry) 
+  and u.state in ('CA', 'LA', 'NY', 'IL')
+  group by 1, 2, 3, 4
+  order by 1, 2, 3
+)
+select
+f.state,
+f.county,
+uc.aland,
+uc.awater,
+ST_Area(uc.geom) as total,
+sum(s.urban) as urban,
+sum(s.urban)/ST_Area(uc.geom) as uratio
+from summary.fmhl f
+left outer join s using (state, county)
+left outer join fima.us_county_2015 uc on (f.state = uc.state) and (f.county = uc.name)
+group by 1, 2, 3, 4, 5
+order by 1, 2;
+
+alter table summary.fmhl_urban add primary key (state, county);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
