2809242ad3122056be524a2c17b29cc4663f4ea4
ruihui0821 <ruihui0821@users.noreply.github.com>
2017-09-01 14:23:27 -0700

    Update ca_policy_statistics.sql

--- ca_policy_statistics.sql
+++ ca_policy_statistics.sql
@@ -4,8 +4,8 @@ SAN FRANCISCO, CITY AND COUNTY OF|    132 |  36638900 |   78098
 
 (need to update in the imported csv)
 
-drop table ca.ca_policy_20170331;
-create table ca.ca_policy_20170331 as
+drop table ca.ca_policy_20170630;
+create table ca.ca_policy_20170630 as
 select
 n.cid,
 p.*,
@@ -16,12 +16,13 @@ left outer join fima.nation n on (p.community = n.community_name)
 left outer join fima.jurisdictions j on (n.cid = j.j_cid)
 where n.state = 'CA' and j.j_statefp10 = '06';
 
-alter table ca.ca_policy_20170331 add primary key (cid);
-select * from ca.policydata p where p.community not in (select c.community from ca.ca_policy_20170331 c where c.community is not null);
+alter table ca.ca_policy_20170630 add primary key (cid);
+select * from ca.policydata p where p.community not in (select c.community from ca.ca_policy_20170630 c where c.community is not null);
            community            | policy | insurance | premium 
 --------------------------------+--------+-----------+---------
- RL MONTE, CITY OF              |      6 |   1645000 |    2110
- AGUA CALIENTE BAND OF CAHUILLA |    159 |  40740400 |   79261
+ RL MONTE, CITY OF              |      5 |   1295000 |    1737
+ AGUA CALIENTE BAND OF CAHUILLA |    158 |  42126800 |   91460
+ JURUPA VALLEY, CITY OF         |      2 |    700000 |     928
 
 (need to manually add to the table)
 
@@ -30,29 +31,30 @@ RL MONTE, CITY OF: cid = '060658'
 AGUA CALIENTE BAND OF CAHUILLA: cid = '060763' use '060245'
 (060763 AGUA CALIENTE BAND OF CAHUILLA INDIANS TRIBE, RIVERSIDE COUNTY, 
  USE THE RIVERSIDE COUNTY [060245] FIRM AND USE THE CITIES OF CATHEDRAL CITY [060704] AND PALM SPRINGS [060257] FIRMS.)
+060286# JURUPA VALLEY, CITY OF RIVERSIDE COUNTY, Inital FIRM Identified 08/18/14, not exists in jurisdictions table
 
-update ca.ca_policy_20170331 c
+update ca.ca_policy_20170630 c
 set boundary = (
 select j.boundary 
 from fima.jurisdictions j
 where j.j_cid = '060658')
 where c.cid =  '060658';
 
-update ca.ca_policy_20170331 c
+update ca.ca_policy_20170630 c
 set population = (
 select j. j_pop10 
 from fima.jurisdictions j
 where j.j_cid = '060658')
 where c.cid =  '060658';
 
-update ca.ca_policy_20170331 c
+update ca.ca_policy_20170630 c
 set boundary = (
 select j.boundary 
 from fima.jurisdictions j
 where j.j_cid = '060245')
 where c.cid =  '060763';
 
-update ca.ca_policy_20170331 c
+update ca.ca_policy_20170630 c
 set population = (
 select j. j_pop10 
 from fima.jurisdictions j
@@ -64,7 +66,7 @@ n.county,
 sum(policy) as policy,
 sum(insurance) as insurance,
 sum(premium) as premium
-from ca.ca_policy_20170331 c
+from ca.ca_policy_20170630 c
 join fima.nation n using (cid)
 group by 1
 order by 1;
